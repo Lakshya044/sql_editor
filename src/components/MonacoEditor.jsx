e71b9b2904fea7fd16f57bfe9f5187aa13dc6db7
Lakshya <harshitlucky2004@gmail.com>
2025-04-02 18:18:58 +0530

    Monacoeditor dynamic font size depends on screen size

--- src/components/MonacoEditor.jsx
+++ src/components/MonacoEditor.jsx
@@ -1,10 +1,34 @@
-'use client';
+
+"use client";
+import { useState, useEffect } from "react";
 import Editor from "@monaco-editor/react";
 import useQueryStore from "@/app/store";
-import '@/styles/MonacoEditor.css'
+import "@/styles/MonacoEditor.css";
 
 const MonacoEditor = () => {
   const { currentQuery, setQuery } = useQueryStore();
+  const [fontSize, setFontSize] = useState(18); 
+
+ 
+  const updateFontSize = () => {
+    const screenWidth = window.innerWidth;
+    if (screenWidth > 1200) {
+      setFontSize(18);
+    } else if (screenWidth > 900) {
+      setFontSize(16);
+    } else if (screenWidth > 600) {
+      setFontSize(14);
+    } else {
+      setFontSize(12);
+    }
+  };
+
+  
+  useEffect(() => {
+    updateFontSize(); 
+    window.addEventListener("resize", updateFontSize);
+    return () => window.removeEventListener("resize", updateFontSize);
+  }, []);
 
   return (
     <div className="monaco-editor-container">
@@ -15,7 +39,7 @@ const MonacoEditor = () => {
         onChange={(value) => setQuery(value)}
         options={{
           minimap: { enabled: false },
-          fontSize: 18,
+          fontSize: fontSize, 
           automaticLayout: true,
         }}
       />
